b8a84b2ff4beb73e98c73057feb219db2dd58579
Leonardo <leo.lima21051990@gmail.com>
2020-02-14 15:23:21 -0300

    rede neural de 1 camada

---
+++ perceptrons/perceptron3.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+entradas = np.array([[0, 0], [0,1], [1,0], [1,1]])
+saidas = np.array([0, 0, 0, 1])
+pesos = np.array([0.0, 0.0])
+taxaAprendizagem = 0.1
+
+def stepFunction(soma):
+  if(soma >= 1):
+    return 1
+  return 0
+
+def calculaSaida(registro):
+  s = registro.dot(pesos)
+  return stepFunction(s)
+
+def treinar():
+  erroTotal = 1
+  while (erroTotal != 0):
+    erroTotal = 0
+    for i in range(len(saidas)):
+      saidaCalculada = calculaSaida(np.asarray(entradas[i]))
+      erro = abs(saidas[i] - saidaCalculada)
+      erroTotal += erro
+      for j in range (len(pesos)):
+        pesos[j] = pesos[j] + (taxaAprendizagem * entradas[i][j] * erro)
+        print('peso atualizado: ' + str(pesos[j]))
+
+    print('total de erros: ' + str(erroTotal))
+
+
+treinar() 
+print('rede neural treinada')
+print(calculaSaida(entradas[0]))
+print(calculaSaida(entradas[1]))
+print(calculaSaida(entradas[2]))
+print(calculaSaida(entradas[3]))
